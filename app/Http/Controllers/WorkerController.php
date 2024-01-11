dea29595e1a3b7dd41172a8d1c3907d46f22d966
ANkulagin <ankulskull@gmail.com>
2024-01-11 20:40:10 +0300

    Добавлен поиск по фильтрам IndexRequest.php index.blade.php WorkerController.php

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Worker\IndexRequest;
 use App\Http\Requests\Worker\StoreRequest;
 use App\Http\Requests\Worker\UpdateRequest;
 use App\Models\Worker;
@@ -9,13 +10,57 @@ use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    private $count=1;
-    public function index()
+
+    public function index(IndexRequest $request)
     {
-        //pagination
-        $workers = Worker::paginate(4);
-        return view('worker.index',compact('workers'));
+        // Валидация данных запроса с использованием IndexRequest
+        $data = $request->validated();
+
+        // Создание запроса к базе данных для модели Worker
+        $workerQuery = Worker::query();
+
+        // Фильтрация по имени
+        if (isset($data['name'])){
+            $workerQuery->where('name', 'like', "%{$data['name']}%");
+        }
+
+        // Фильтрация по фамилии
+        if (isset($data['surname'])){
+            $workerQuery->where('surname', 'like', "%{$data['surname']}%");
+        }
+
+        // Фильтрация по email
+        if (isset($data['email'])){
+            $workerQuery->where('email', 'like', "%{$data['email']}%");
+        }
+
+        // Фильтрация по возрасту (от)
+        if (isset($data['from'])){
+            $workerQuery->where('age', '>', $data['from']);
+        }
+
+        // Фильтрация по возрасту (до)
+        if (isset($data['to'])){
+            $workerQuery->where('age', '<', $data['to']);
+        }
+
+        // Фильтрация по описанию
+        if (isset($data['description'])){
+            $workerQuery->where('description', 'like', "%{$data['description']}%");
+        }
+
+        // Фильтрация по статусу семейного положения (если выбрано)
+        if (isset($data['is_married'])){
+            $workerQuery->where('is_married', true);
+        }
+
+        // Пагинация - получение результатов в формате "страниц"
+        $workers = $workerQuery->paginate(4);
+
+        // Возвращение представления с передачей данных о сотрудниках
+        return view('worker.index', compact('workers'));
     }
+
 
     public function show(Worker $worker)
     {
---
+++ app/Http/Requests/Worker/IndexRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Worker;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'nullable|string',
+            'surname' => 'nullable|string',
+            'email' => 'nullable|email',
+            'from' => 'nullable|integer',
+            'to' => 'nullable|integer',
+            'description' => 'nullable|string',
+            'is_married' => 'nullable|string ',
+        ];
+    }
+}
