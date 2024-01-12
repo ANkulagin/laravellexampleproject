5c78b0094b76cfe37f8846bebb163df15d982650
ANkulagin <ankulskull@gmail.com>
2024-01-12 20:17:55 +0300

    комменты обновил/добавил

--- app/Http/Requests/Worker/IndexRequest.php
+++ app/Http/Requests/Worker/IndexRequest.php
@@ -7,13 +7,27 @@ use Illuminate\Foundation\Http\FormRequest;
 class IndexRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * Определение, может ли пользователь сделать этот запрос.
+     * В данном случае всегда возвращает true, что означает,
+     * что запрос авторизован всегда без дополнительной проверки.
+     * В реальном проекте, возможно, потребуется добавить более сложную логику
+     * в этот метод для проверки прав доступа.
      */
     public function authorize(): bool
     {
         return true;
     }
 
+    /**
+     * Правила валидации для параметров запроса.
+     * Определяются правила валидации для каждого параметра запроса.
+     * В данном случае:
+     * - 'name' и 'surname' должны быть строками или null,
+     * - 'email' должен быть допустимым адресом электронной почты или null,
+     * - 'from' и 'to' должны быть целыми числами или null,
+     * - 'description' должен быть строкой или null,
+     * - 'is_married' должен быть строкой или null.
+     */
     public function rules(): array
     {
         return [
--- resources/views/worker/index.blade.php
+++ resources/views/worker/index.blade.php
@@ -1,61 +1,69 @@
-<!doctype html>
-<html lang=ru>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Список работников</title>
 </head>
 <body>
+<!-- Заголовок страницы -->
 INDEX
 <div>
-
-
+    <!-- Ссылка для создания нового работника -->
     <div>
-        <a href="{{route('worker.create')}}">Создать работника</a>
+        <a href="{{ route('worker.create') }}">Создать работника</a>
     </div>
     <hr>
+    <!-- Форма для фильтрации списка работников -->
     <div>
-        <form action="{{route('worker.index')}}">
+        <form action="{{ route('worker.index') }}" method="get">
+            <!-- Поля для фильтрации -->
             <input type="text" name="name" placeholder="name" value="{{ request()->get('name') }}">
             <input type="text" name="surname" placeholder="surname" value="{{ request()->get('surname') }}">
             <input type="text" name="email" placeholder="email" value="{{ request()->get('email') }}">
             <input type="number" name="from" placeholder="from" value="{{ request()->get('from') }}">
             <input type="number" name="to" placeholder="to" value="{{ request()->get('to') }}">
             <input type="text" name="description" placeholder="description" value="{{ request()->get('description') }}">
-            <input type="checkbox" name="is_married" id="isMarried" value="{{ request()->get('is_married') }}">
+            <input type="checkbox" name="is_married" id="isMarried" {{ request()->get('is_married') ? 'checked' : '' }}>
             <label for="isMarried">в браке/не в браке</label>
+            <!-- Кнопка для отправки формы -->
             <input type="submit">
-            <a href="{{route('worker.index')}}">снять фильтры</a>
+            <!-- Ссылка для снятия фильтров -->
+            <a href="{{ route('worker.index') }}">снять фильтры</a>
         </form>
     </div>
     <hr>
+    <!-- Вывод списка работников -->
     @foreach($workers as $worker)
         <div>
-            <div>{{$worker->name}}</div>
-            <div>{{$worker->surname}}</div>
-            <div>{{$worker->email}}</div>
-            <div>{{$worker->age}}</div>
+            <div>{{ $worker->name }}</div>
+            <div>{{ $worker->surname }}</div>
+            <div>{{ $worker->email }}</div>
+            <div>{{ $worker->age }}</div>
+            <!-- Ссылка для просмотра деталей работника -->
             <div>
-                <a href="{{ route('worker.show',['worker'=> $worker->id]) }}">Просмотреть</a>
+                <a href="{{ route('worker.show', ['worker' => $worker->id]) }}">Просмотреть</a>
             </div>
+            <!-- Ссылка для редактирования информации о работнике -->
             <div>
-                <a href="{{ route('worker.edit',['worker'=> $worker->id]) }}">Редактировать</a>
+                <a href="{{ route('worker.edit', ['worker' => $worker->id]) }}">Редактировать</a>
             </div>
+            <!-- Форма для удаления работника -->
             <div>
-                <form action="{{route('worker.delete',$worker->id)}}" method="post">
+                <form action="{{ route('worker.delete', $worker->id) }}" method="post">
                     @csrf
                     @method('delete')
+                    <!-- Кнопка для удаления работника -->
                     <input type="submit" value="Удалить">
                 </form>
             </div>
         </div>
         <hr>
     @endforeach
-    //pagination
+    <!-- Пагинация -->
     <div>
-        {{ $workers->links()}}
+        {{ $workers->links() }}
     </div>
 </div>
 </body>
